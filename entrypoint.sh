27fe6baa90531dab0fc80b806547ea13f17a303a
Meng Sen <qyg2297248353@gmail.com>
2024-11-01 18:03:17 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# 检查持久化目录是否存在，如果不存在则拷贝项目文件
+chmod +x /entrypoint.sh
+
 if [ ! -d "/var/www/html" ]; then
     cp -r /usr/src/nexusphp/* /var/www/html/
-    chown -R www-data:www-data /var/www/html
-    chmod -R 0777 /var/www/html
 fi
 
-# 启动 supervisord 以管理服务
+chown -R www-data:www-data /var/www/html
+
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
