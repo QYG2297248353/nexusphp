3203073af7a7f6106a3773b181623448c7f36c6e
Meng Sen <qyg2297248353@gmail.com>
2024-11-04 15:37:54 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-chmod +x /entrypoint.sh
-
-if [ ! -d "/var/www/html" ]; then
-    cp -r /usr/src/nexusphp/* /var/www/html/
+if [ -z "$(ls -A /var/www/html)" ]; then
+    cp -R /tmp/app/* /var/www/html/
 fi
 
-chown -R www-data:www-data /var/www/html
-
-exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
+exec /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
