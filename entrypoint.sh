15af933b4be0c9610c1b20e7a3c65959812ceba5
Meng Sen <qyg2297248353@gmail.com>
2024-11-04 18:31:05 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-for file in /tmp/app/*; do
-    if [ ! -e "/var/www/html/$(basename "$file")" ]; then
-        cp -R "$file" /var/www/html/
-    fi
-done
+cp -R /tmp/app/* /var/www/html/ 2>/dev/null
 
 exec /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
