f72e665cb47bbe1d722dd0c06398a0793d1d5807
Meng Sen <qyg2297248353@gmail.com>
2024-11-05 16:09:52 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,11 @@ cp -Rn /tmp/app/. /var/www/html/
 chown -R www-data:www-data /var/www/html
 chmod -R 777 /tmp
 
+if [ -f /var/www/html/install.lock ]; then
+    echo "初始化已完成，删除 /var/www/html/public/install 目录"
+    rm -rf /var/www/html/public/install
+else
+    echo "请尽快完成站点初始化工作"
+fi
+
 exec /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
