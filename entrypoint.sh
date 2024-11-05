8a0e8642c2c09dc03aa1e076e947e47edf9e23ad
Meng Sen <qyg2297248353@gmail.com>
2024-11-05 13:26:20 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-cp -Rn /tmp/app/* /var/www/html/
+cp -Rn /tmp/app/. /var/www/html/
 chown -R www-data:www-data /var/www/html
+chmod -R 777 /tmp
 
 exec /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
